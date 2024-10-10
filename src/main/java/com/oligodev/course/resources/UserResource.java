444d9df4a10a0f55e88cb7c196acf8f5244c4e3b
Andre Gomes <andre.gomes@autoeclick.com>
2024-10-10 00:48:05 -0300

    User entity and resource

---
+++ src/main/java/com/oligodev/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.oligodev.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.oligodev.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		 User user = new User(1L, "André", "andreteste@gmail.com", "999999999", "12345");
+		 return ResponseEntity.ok().body(user);
+	}
+}
